d8a175741ec3d7cb35e69971b876dcb945b51a33
Igor Shevchenko <cyberskunk@gmail.com>
2014-11-04 20:30:57 +0200

    add another test

--- test/noParam.js
+++ test/noParam.js
@@ -11,6 +11,7 @@ exports.test = function (test) {
     test.strictEqual(typeof cssbeautifyCli.options, 'undefined', 'bad options ' + JSON.stringify(cssbeautifyCli.options));
 
     test.strictEqual(typeof cssbeautifyCli.filename, 'undefined', 'bad filename');
+    test.strictEqual(typeof cssbeautifyCli.stdin, 'undefined', 'bad stdin option');
 
     test.strictEqual(typeof cssbeautifyCli.exit, 'object', 'bad exit object');
     test.strictEqual(cssbeautifyCli.exit.code, 1, 'bad exit code');
